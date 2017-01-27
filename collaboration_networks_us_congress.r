632683964e742df7c85399b3aecfb3c180305b60
goto4711 <tobias.blanke@gmail.com>
2017-01-27 21:13:03 +0000

    Update collaboration_networks_us_congress.r

--- collaboration_networks_us_congress.r
+++ collaboration_networks_us_congress.r
@@ -60,6 +60,7 @@ colnames(sponsor_matrix) <- all_sponsors_cosponsors
 rownames(sponsor_matrix) <- all_bills
 
 for(i in 1:length(bill_sponsors_list)){
+    print(i)
     sponsor_matrix[i, which(all_sponsors_cosponsors == bill_sponsors_list[[i]]$sponsor)] <- "Sponsor"
     if(length(bill_sponsors_list[[i]]$cosponsors) > 0){
     	for(j in 1:length(bill_sponsors_list[[i]]$cosponsors)){
