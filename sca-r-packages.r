d96e65b1fc8ff4e565310392391253601bc24720
goto4711 <tobias.blanke@gmail.com>
2018-08-19 14:15:20 +0100

    Update sca-r-packages.r

--- sca-r-packages.r
+++ sca-r-packages.r
@@ -9,16 +9,16 @@ ipak <- function(pkg){
 
 #Get the Tidyverse
 
-tidy_pack <- c("devtools", "tidyverse", "stringr", "plyr", "lubridate")
+#tidy_pack <- c("devtools", "tidyverse", "stringr", "plyr", "lubridate")
 
-ipak(tidy_pack)
+ipak("tidyverse")
 
 #Retrieve from dockerfile:
 #Replace leading white spaces with nothing
 #Replace whitespace + \ with ",
 #Switch to grep mode and replace \n with empty space
 
-course_packages <- c("GISTools", "OpenStreetMap", "NeuralNetTools", "NeuralNetTools", "RColorBrewer", "RedditExtractoR", "Rfacebook", "XML", "arules", "catdata", "cluster", "corrplot", "devtools", "dplyr", "fitdistrplus", "gapminder", "ggmap", "ggplot2", "ggraph", "ggrepel", "ggvis", "googleVis", "graphTweets", "gsheet", "gtrendsR", "gutenbergr", "httr", "imputeTS", "igraph", "jpeg", "jsonlite", "lubridate", "magrittr", "maps", "nnet", "pageviews", "plotly", "quanteda", "raster", "recommenderlab", "reshape2", "rpart", "rtweet", "rworldmap", "shiny", "spatstat", "swirl", "tidytext", "tm", "tmap", "topicmodels", "twitteR", "wikipediatrend", "wordcloud", "xts")
+course_packages <- c("GISTools", "OpenStreetMap", "NeuralNetTools", "NeuralNetTools", "RColorBrewer", "RedditExtractoR", "Rfacebook", "XML", "arules", "catdata", "cluster", "corrplot", "devtools", "dplyr", "fitdistrplus", "gapminder", "ggmap", "ggplot2", "ggraph", "ggrepel", "ggvis", "googleVis", "graphTweets", "gsheet", "gtrendsR", "gutenbergr", "httr", "imputeTS", "igraph", "jpeg", "jsonlite", "lubridate", "magrittr", "maps", "nnet", "pageviews", "plotly", "quanteda", "raster", "recommenderlab", "reshape2", "rpart", "rtweet", "rworldmap", "shiny", "spatstat", "swirl", "tidytext", "tm", "tmap", "topicmodels", "twitteR", "wikipediatrend", "wordcloud", "xts", "MASS", "mlogit", "caret", "survival")
 
 ipak(course_packages)
 
